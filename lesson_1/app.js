4ffbf029b5a57cd145b8b5d5818419d4fa3cd5aa
MLDODSON <paola@dominicansoul.com>
2023-10-01 19:05:19 -0500

    Branch - 1
    Course setup and Lesson 1 - JS Basics.

---
+++ lesson_1/app.js
@@ -0,0 +1,18 @@
+// EXERCISE 1 //
+alert('Good Morning!');
+alert('My name is Mighty Mike!');
+console.log(10 + 5);
+console.log(20 - 5);
+console.log(2 + 2 - 5);
+document.body.innerHTML = ('Good Morning!');
+document.body.innerHTML = ('I am the Lord of the Rings! Bow down!');
+document.body.innerHTML = ('');
+
+let cost = 10 + 8 + 20;
+console.log(`Yesterday I spent $${cost} on dinner and some clothes.`);
+
+let bankBalance = 100;
+let eatingOut = 70;
+let pay = 200;
+
+console.log(`As of today I have $${bankBalance + pay - eatingOut} in my bank account.`);
